5bb7db228f025599c0a255b669ba464fc4cd2a5b
angaala <56194879+angaala@users.noreply.github.com>
2026-07-17 03:57:54 +0500

    Внес правки

--- test_first.py
+++ test_first.py
@@ -1,21 +1,25 @@
-def test_google_search(page):
-    """Тест проверяет базовый поиск в Google"""
+import urllib.parse
+
+def test_fm18_search(page):
+    """Тест проверяет базовый поиск в fm18"""
     
-    # 1. Открываем главную страницу Google
-    page.goto("https://google.com")
+    # 1. Открываем главную страницу fm18
+    page.goto("https://fm18.ru/")
     
-    # 2. Проверяем, что заголовок вкладки (Title) содержит слово "Google"
-    assert "Google" in page.title(), f"Ожидали заголовок Google, но получили {page.title()}"
+    # 2. Проверяем, что заголовок вкладки (Title) содержит слово "fm18"
+    assert "Фабрика" in page.title(), f"Ожидали заголовок Фабрика, но получили {page.title()}"
     
     # 3. Находим строку поиска по её имени (атрибуту name="q")
-    search_box = page.locator("textarea[name='q']")
+    search_box = page.locator("input[name='search']")
     
     # 4. Вводим поисковый запрос и нажимаем клавишу Enter
-    search_box.fill("pytest")
-    search_box.press("Enter")
+    search_box.fill("стул")
+    page.locator(".button-search").click()
     
     # 5. Ожидаем, пока страница загрузит результаты поиска
     page.wait_for_load_state("networkidle")
     
+    decoded_url = urllib.parse.unquote(page.url)
+    
     # 6. Проверяем, что в адресе страницы (URL) появилось наше поисковое слово
-    assert "pytest" in page.url, f"Слово 'pytest' не найдено в URL: {page.url}"
+    assert "стул" in decoded_url, f"Слово 'стул' не найдено в URL: {page.url}"
